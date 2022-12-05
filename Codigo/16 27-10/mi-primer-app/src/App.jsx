12c90b61ef67a7f49befa1c81a528f27d46ac633
jcdavidarg <jcdavidarg@gmail.com>
2022-12-05 20:20:57 -0300

    componentes en indexjs

--- Codigo/16 27-10/mi-primer-app/src/App.jsx
+++ Codigo/16 27-10/mi-primer-app/src/App.jsx
@@ -1,12 +1,9 @@
-
-import './App.css';
-
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
- 
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- Codigo/16 27-10/mi-primer-app/src/index.js
+++ Codigo/16 27-10/mi-primer-app/src/index.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Boton from "./Boton";
+import Tareas from "./Tareas";
+import Card from "./Card";
 
-import Boton from "./Boton"
-import Tareas from "./Tareas"
-import Card from "./Card"
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<>
-<Card nombre= "Lionel Messi" fechaNacimiento = {1987} clubes = {["Barcelona", "PSG"]} cromo = {true}/>
-<Card nombre= "Kylian Mbappé" fechaNacimiento = {1998} clubes = {["Monaco","PSG"]} cromo = {false}/>
-<Tareas resuelto = {false}/>
-    <Boton numero= {1} titulo = "Rojo"/>
-    <Boton numero= {2}  titulo = "Verde"/>
-    <Boton numero= {3} titulo = "Azul" />
-    <Boton numero= {4} titulo = "Amarillo" />
-    <Boton numero= {5}  />
-    </>
+  <>
+    <Card
+      nombre="Lionel Messi"
+      fechaNacimiento={1987}
+      clubes={["barcelona, psg"]}
+      cromo={true}
+    />
+    <Card
+      nombre="Killiam Mbapee"
+      fechaNacimiento={1998}
+      clubes={["monaco, psg"]}
+      cromo={false}
+    />
+    <Tareas resuelto={false} />
+    <Boton numero={1} titulo="Rojo" />
+    <Boton numero={2} titulo="Verde" />
+    <Boton numero={3} titulo="Azul" />
+    <Boton numero={4} titulo="Amarillo" />
+    <Boton numero={5} />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
