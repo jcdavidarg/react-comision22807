a5232822f8866d688c154a5835cf833faa1c543e
jcdavidarg <jcdavidarg@gmail.com>
2022-12-05 20:20:37 -0300

    componente Boton

--- Codigo/16 27-10/mi-primer-app/src/Boton.jsx
+++ Codigo/16 27-10/mi-primer-app/src/Boton.jsx
@@ -1,20 +1,11 @@
+import React from "react";
 
-
-
-
-function Boton ({numero,titulo = "default"}) {
-    return (
-<button  onClick = {function(){
-    alert("hiciste click")
-}} >{numero} - {titulo}</button>
-    )
+function Boton({ numero, titulo = "default" }) {
+  return (
+    <button>
+      {numero} - {titulo}
+    </button>
+  );
 }
-
-
-/* function Boton (props) {
-    return (
-<button>{props.numero} - {props.titulo}</button>
-    )
-} */
 
 export default Boton;
