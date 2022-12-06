a85a6c0665fc7fb215fe03f2db5635391a02f2e9
jcdavidarg <jcdavidarg@gmail.com>
2022-12-06 03:16:39 -0300

    metodo fetch

---
+++ Codigo/18 03-11/fetch.js
@@ -0,0 +1,25 @@
+const getnamefetch = (idPost) => {
+  fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}`)
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+
+    .then((post) => {
+      console.log(post);
+      fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+        .then((res) => {
+          console.log(res);
+          return res.json();
+        })
+
+        .then((user) => {
+          console.log(user);
+          console.log(
+            `La persona que escribio el post ${idPost} es ${user.name}`
+          );
+        });
+    });
+};
+
+getnamefetch(20);
