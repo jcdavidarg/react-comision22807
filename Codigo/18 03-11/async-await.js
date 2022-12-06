bea4a1f1021b2af2c2fea61577f857ebd9ae8a6e
jcdavidarg <jcdavidarg@gmail.com>
2022-12-06 03:16:33 -0300

    metodo async-await

---
+++ Codigo/18 03-11/async-await.js
@@ -0,0 +1,24 @@
+const getnameAsync = async (idPost) => {
+  try {
+    const resPost = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${idPost}`
+    );
+    console.log(resPost);
+
+    const post = await resPost.json();
+    console.log(post);
+
+    const respUser = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${post.userId}`
+    );
+    console.log(respUser);
+    const user = await respUser.json();
+    console.log(user);
+
+    console.log(`La persona que escribio el post ${post.id} es ${user.name}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getnameAsync(5);
